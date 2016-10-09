83f016d61b16cc63f4c157131d6585de16654803
Sheng-Loong Su <sushengloong@gmail.com>
2016-10-09 21:32:11 +0800

    Show sensible confirm delete message

--- src/components/confirm_delete_modal.js
+++ src/components/confirm_delete_modal.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import { Button, Modal } from 'react-bootstrap';
 
 export default class ConfirmDeleteModal extends Component {
+  renderMessage() {
+    if (!this.props.card) return "No card selected!";
+
+    return `Are you sure you want to delete card #${this.props.card.id}?`;
+  }
+
   render() {
     return (
       <Modal show={this.props.showConfirmDelete} onHide={this.props.onClickCloseForm}>
         <Modal.Header closeButton>
-          <Modal.Title>Confirm Delete?</Modal.Title>
+          <Modal.Title>Confirm Delete</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          Confirm?
+          {this.renderMessage()}
         </Modal.Body>
         <Modal.Footer>
-          <Button>Close</Button>
+          <Button>No</Button>
+          <Button bsStyle="danger">Yes</Button>
         </Modal.Footer>
       </Modal>
     )
